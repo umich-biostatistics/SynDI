a6a9d9f37089859e65e7117fe530c84f7578d305
Michael Kleinsasser <mkleinsa@umich.edu>
2021-07-13 09:41:49 -0400

    remove now empty file

--- SynDI.R
+++
@@ -1,9 +0,0 @@
-##### Table of Contents:
-# Function 1: Expit function
-# Function 2: Create the synthetic data
-# Function 3: Calculate the initial estimates for external populations
-# Function 4: Resampling function to get bootstrap variance for binary Y in Example 1
-# Function 5: Resampling function to get bootstrap variance for continuous Y in Example 2
-
-# Reference: Gu, T., Taylor, J.M.G. and Mukherjee, B. (2021) Regression inference for multiple populations by integrating summary-level data using stacked imputations
-#            <arXiv:http://arxiv.org/abs/2106.06835>. 
